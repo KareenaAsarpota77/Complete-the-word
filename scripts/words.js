b125a636c285a27a2afc23d9d486dab2513b50f2
Kareena <kareena.asar@gmail.com>
2023-09-30 17:11:24 +0530

    deploy

---
+++ scripts/words.js
@@ -0,0 +1,122 @@
+const wordList = [
+    {
+        word: "equality",
+        hint: "A situation in which everyone is equal and has the same rights."
+    },
+    {
+        word: "freedom",
+        hint: "The condition or right of being able or allowed to do whatever you want to, without being controlled or limited."
+    },
+    {
+        word: "immunity",
+        hint: "The condition of being protected from the law."
+    },
+    {
+        word: "lawyer",
+        hint: "Someone who speaks up for a child's rights. A person who is professionally qualified to plead the cause of another in a court of law."
+    },
+    {
+        word: "expression",
+        hint: "The right to speak your mind. The Freedom of ..."
+    },
+    {
+        word: "custody",
+        hint: "The legal right to care for someone, especially a child after its parents have separated."
+    },
+    {
+        word: "shelter",
+        hint: "A safe place to protect yourself from bad weather, danger, or attack."
+    },
+    {
+        word: "judge",
+        hint: "A person who makes sure that everyone follows the rules and makes fair decisions."
+    },
+    {
+        word: "case",
+        hint: "A puzzle or mystery that the judge needs to solve using special rules."
+    },
+    {
+        word: "crime",
+        hint: "When someone breaks a big rule that's not allowed."
+    },
+    {
+        word: "arrest",
+        hint: "When the police take someone to talk to them about a big rule they might have broken."
+    },
+    {
+        word: "innocent",
+        hint: "When someone didn't do anything wrong, and it's proven."
+    },
+    {
+        word: "bail",
+        hint: "Like a special promise to the judge that you'll come back to court if you're allowed to leave for a little while."
+    },
+    {
+        word: "rights",
+        hint: "Things that you are allowed to have or do because you are a person."
+    },
+    {
+        word: "friendship",
+        hint: "Making and having good relationships with other kids."
+    },
+    {
+        word: "police",
+        hint: "A person who helps keep everyone safe and makes sure people follow the rules."
+    },
+    {
+        word: "health",
+        hint: "Taking care of your body and making sure you're not sick or hurt"
+    },
+    {
+        word: "voice",
+        hint: "Being able to say what you think or feel about something."
+    },
+    {
+        word: "family",
+        hint: "The people who take care of you and love you, like parents, siblings, and guardians."
+    },
+    {
+        word: "respect",
+        hint: "Treating others the way you want to be treated, with kindness and politeness."
+    },
+    {
+        word: "sharing",
+        hint: "Taking turns and letting others use or play with your things."
+    },
+    {
+        word: "protection",
+        hint: "Keeping someone safe from harm or danger."
+    },
+    {
+        word: "responsibility",
+        hint: "Doing the things you're supposed to do, like chores and homework."
+    },
+    {
+        word: "understanding",
+        hint: "Getting why something happens or how someone feels."
+    },
+    {
+        word: "privacy",
+        hint: "Having some personal space and keeping some things just for you."
+    },
+    {
+        word: "vote",
+        hint: "Choosing who you want to lead your country or community."
+    },
+    {
+        word: "choice",
+        hint: "Being able to pick what you want to do or be, without someone else deciding for you."
+    },
+    {
+        word: "belongings",
+        hint: "The things that are yours, like toys, books, and clothes"
+    },
+    {
+        word: "abuse",
+        hint: "When someone hurts or treats a child very badly."
+    },
+    {
+        word: "healthcare",
+        hint: "Access to doctors and medicine."
+    }
+];
